d2de494b73db5466ae04ede54fdd7a962a045211
Aleksander Evensen <ahse03@gmail.com>
2026-02-12 10:31:29 +0100

    Monorepo setup

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,4 +1,4 @@
-import type { BackendApi } from "drift-backend";
+import type { BackendApi } from "@drift/backend";
 import { hc } from "hono/client";
 
 export const apiClient = hc<BackendApi>("http://localhost:3000/");
